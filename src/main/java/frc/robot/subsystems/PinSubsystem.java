83a150a8c1aa2cc67f4dcf4e4632f65456a3b0a1
OVHSRobotics4619 <ovhsrobotics@gmail.com>
2024-03-17 12:36:52 -0700

    Make pin subsystem

---
+++ src/main/java/frc/robot/subsystems/PinSubsystem.java
@@ -0,0 +1,22 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import com.ctre.phoenix.motorcontrol.TalonSRXControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+/** A hatch mechanism actuated by a single {@link DoubleSolenoid}. */
+public class PinSubsystem extends SubsystemBase {
+  private TalonSRX pinMotor = new TalonSRX(14);
+
+  public PinSubsystem() {
+  }
+
+  public void enablePin() {
+    pinMotor.set(TalonSRXControlMode.PercentOutput, 1);
+  }
+
+  public void disablePin() {
+    pinMotor.set(TalonSRXControlMode.PercentOutput, 0);
+  }
+}
