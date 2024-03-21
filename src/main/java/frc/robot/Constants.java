e02e57bbbeab461165ac22dfc8e544f758f487df
OVHSRobotics4619 <ovhsrobotics@gmail.com>
2024-03-20 18:01:12 -0700

    mess with camera angles

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -124,7 +124,7 @@ public final class Constants
     public static final double TURN_STOP_LIMIT = 1;
 
     // The layout of the AprilTags on the field
-    public static final AprilTagFieldLayout kTagLayout = AprilTagFields.k2024Crescendo.loadAprilTagLayoutField();
+    public static final AprilTagFieldLayout kTagLayout = AprilTagFields.kDefaultField.loadAprilTagLayoutField();
 
     // The standard deviations of our vision estimated poses, which affect correction rate
     // (Fake values. Experiment and determine estimation noise on an actual robot.)
@@ -141,7 +141,7 @@ public final class Constants
       
       // Cam mounted facing forward, half a meter forward of center, half a meter up from center.
       public static final Transform3d kRobotToCam =
-            new Transform3d(new Translation3d(Units.inchesToMeters(11), Units.inchesToMeters(-1.5), Units.inchesToMeters(12.5)), new Rotation3d(0, Math.toRadians(12), 0));
+            new Transform3d(new Translation3d(Units.inchesToMeters(11), Units.inchesToMeters(-1.5), Units.inchesToMeters(12.5)), new Rotation3d(0, Math.toRadians(12), Math.toRadians(90)));
     }
   }
 }
